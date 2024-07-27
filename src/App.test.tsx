57cb019cc92cfd1480e4c6680ca5f258dcece6ae
Wizard <rcvbuf@gmail.com>
2024-07-27 08:40:46 +0800

    fix: build

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './popup/Popup';
-
+import '@testing-library/jest-dom';
+   
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeInTheDocument(): R;
+    }
+  }
+}
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
